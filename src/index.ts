d1923cce9e4cdf4f8c7d830494476658dd88f859
Scott White <stolidwisdom@gmail.com>
2022-02-09 08:27:34 -0500

    Add DB entries using MikroORM

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("hello there");
+import { MikroORM } from "@mikro-orm/core";
+
+const main = async () => {
+    const orm = await MikroORM.init({
+        dbName: 'lireddit',
+        type: 'postgresql',
+        debug: process.env.NODE_ENV
+    });
+
+    console.log("hello there");
+}
+
+main();
+
