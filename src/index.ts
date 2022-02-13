664c53f63dfc30d6ce934bc5b62ac90992efe59d
Scott White <stolidwisdom@gmail.com>
2022-02-13 10:58:09 -0500

    entities

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
-import { MikroORM } from "@mikro-orm/core";
+import { compareBuffers, MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
 
 const main = async () => {
     const orm = await MikroORM.init({
+        entities: [Post],
         dbName: 'lireddit',
         type: 'postgresql',
-        debug: process.env.NODE_ENV
+        debug: !__prod__
     });
 
-    console.log("hello there");
-}
+    const post = orm.em.create(Post, {title: 'my first post'});
+    await orm.em.persistAndFlush(post);
+    await orm.em.nativeInsert(Post, {title: 'my first post 2'};)
 
 main();
 
